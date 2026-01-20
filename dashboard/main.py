efb22e4683f2dd0d32d98eaab9d06663aecd440c
Aryan Nagpal <an125@snu.edu.in>
2026-01-20 08:53:02 +0530

    changed sessions endpoint

--- dashboard/main.py
+++ dashboard/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 from supabase import create_client
 from dotenv import load_dotenv
-
+from fastapi import HTTPException
 load_dotenv()
 
 SUPABASE_URL = os.getenv("SUPABASE_URL")
@@ -14,7 +14,9 @@ supabase = create_client(SUPABASE_URL, SUPABASE_SECRET_KEY)
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://frontend-nine-ochre-93.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -24,6 +26,19 @@ app.add_middleware(
 def get_sessions():
     return supabase.table("sessions").select("*").order("created_at", desc=True).execute().data
 
+
 @app.get("/sessions/{session_id}")
 def get_session(session_id: str):
-    return supabase.table("sessions").select("*").eq("room_id", session_id).single().execute().data
+    res = (
+        supabase
+        .table("sessions")
+        .select("*")
+        .eq("room_id", session_id)
+        .execute()
+    )
+
+    if not res.data:
+        raise HTTPException(status_code=404, detail="Session not found")
+
+    return res.data[0]
+
